7cfceef339c8bd2461ddcb48ab562863ad998fb7
Imrul Hassan Khan <imrulhassankhanporosh@gmail.com>
2024-12-23 06:10:20 +0600

    updated external script file and added 2 new files for statement and variable of JS

---
+++ src/statement.js
@@ -0,0 +1,9 @@
+
+alert("hello without semicolon")
+
+
+/* it is possible to leave out semicolons most of the time. But it’s safer – especially for a beginner – to
+ use them.*/
+
+alert("hello");
+alert("World!");
